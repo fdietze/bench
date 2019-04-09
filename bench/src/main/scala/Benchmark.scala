b4074f230836dc6a88c7e9bbe708ebf394bc0d63
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-04-09 15:36:02 +0200

    Remove BenchmarkCustomIterations

--- bench/src/main/scala/Benchmark.scala
+++ bench/src/main/scala/Benchmark.scala
@@ -3,9 +3,9 @@ package bench
 import scala.concurrent.duration._
 import util._
 
-trait BenchmarkLike[T] {
+sealed trait BenchmarkLike[T] {
   def name: String
-  def init: Int => T
+  def init: Int => T // TODO: remove init from trait
   def runWithInit(size: Int, iterations: Long)
 }
 
@@ -16,10 +16,6 @@ case class BenchmarkWithoutInit(name: String, code: (Int) => Any) extends Benchm
 
 case class Benchmark[T](name: String, init: Int => T, code: (T) => Any) extends BenchmarkLike[T] {
   def runWithInit(size: Int, iterations: Long) = loop(iterations) { code(init(size)) }
-}
-
-case class BenchmarkCustomIterations[T](name: String, init: Int => T, code: (T, Long) => Any) extends BenchmarkLike[T] {
-  def runWithInit(size: Int, iterations: Long) = code(init(size), iterations)
 }
 
 case class Comparison(name: String, benchmarks: Seq[BenchmarkLike[_]])
